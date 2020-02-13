6d627f92228c3f2a96b261c94812bcee97b181f6
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-02-13 22:16:10 +0530

    added photo gallery

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
+import Mid from './src/components/Mid'
+import Body from './src/components/Body'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Mid />
+      <Body />
     </View>
   );
 }
